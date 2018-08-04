853dbd34b44fcfb3cdb5a982d241846b67c46cf1
Heshamovic <mohamed.97198@gmail.com>
2018-08-04 14:46:32 +0200

    Adding some losing and winning states

--- app/src/main/java/com/example/project/hardcalculator/LevelsActivity.kt
+++ app/src/main/java/com/example/project/hardcalculator/LevelsActivity.kt
@@ -1,9 +1,16 @@
 package com.example.project.hardcalculator
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import android.R.string.cancel
+import android.content.DialogInterface
+import android.widget.Button
+
 
 class LevelsActivity : AppCompatActivity() {
 
@@ -17,15 +24,29 @@ class LevelsActivity : AppCompatActivity() {
     var minusNumber = 5
     var divideNumber = 5
     var multplyNumber = 5
-    var InitNumber = 5
+    var InitNumber = 1
     var moves = 7
-    fun ResetLevel(view: View)
+    var FinalNumber = 25
+    fun Reset ()
     {
         currentNumber = InitNumber
         moves = 7
+        val divBtn = findViewById<View>(R.id.Divide_button_view) as Button
+        val mulBtn = findViewById<View>(R.id.Multply_button_view) as Button
+        val minusBtn = findViewById<View>(R.id.Minus_button_view) as Button
+        val plusBtn = findViewById<View>(R.id.Plus_button_view) as Button
+        val signBtn = findViewById<View>(R.id.changeSign_button_view) as Button
+        divBtn.isEnabled = true
+        mulBtn.isEnabled = true
+        minusBtn.isEnabled = true
+        plusBtn.isEnabled = true
+        signBtn.isEnabled = true
         display(currentNumber)
     }
-
+    fun ResetLevel(view: View)
+    {
+        Reset()
+    }
     fun Plus(view: View)
     {
         moves--
@@ -59,15 +80,63 @@ class LevelsActivity : AppCompatActivity() {
         currentNumber *= -1
         display(currentNumber)
     }
-
     private fun display(number: Int) {
         val currentNumberTextView = findViewById<View>(R.id.Number_text_view) as TextView
         currentNumberTextView.text = "" + number
-        if (moves < 0)
-        {
-            moves = 0
-        }
         val currentMoves = findViewById<View>(R.id.LeftMoves_text_view) as TextView
         currentMoves.text = "Left Moves : " + moves
+        if (currentNumber == FinalNumber || moves == 0)
+            EndGame(moves);
+    }
+    private fun EndGame(Number: Int)
+    {
+        val builder = AlertDialog.Builder(this@LevelsActivity)
+        if (currentNumber == FinalNumber)
+        {
+            builder.setMessage("Congratulations!! Won in " + ( 7 - moves) + " moves!!")
+            builder.setNegativeButton("Try Again"){dialog, which ->
+                Reset()
+                Toast.makeText(applicationContext,"Try Again",Toast.LENGTH_LONG).show()
+            }
+            builder.setNeutralButton("OK"){_,_ ->
+                val divBtn = findViewById<View>(R.id.Divide_button_view) as Button
+                val mulBtn = findViewById<View>(R.id.Multply_button_view) as Button
+                val minusBtn = findViewById<View>(R.id.Minus_button_view) as Button
+                val plusBtn = findViewById<View>(R.id.Plus_button_view) as Button
+                val signBtn = findViewById<View>(R.id.changeSign_button_view) as Button
+                divBtn.isEnabled = false
+                mulBtn.isEnabled = false
+                minusBtn.isEnabled = false
+                plusBtn.isEnabled = false
+                signBtn.isEnabled = false
+            }
+            builder.setPositiveButton("Next Level"){dialog, which ->
+                Toast.makeText(applicationContext, "El mfrod f hna next level", Toast.LENGTH_LONG).show()
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+        else if (moves == 0)
+        {
+            builder.setMessage("Loser !!")
+            builder.setPositiveButton("Try Again"){dialog, which ->
+                Reset()
+                Toast.makeText(applicationContext,"Try Again",Toast.LENGTH_SHORT).show()
+            }
+            builder.setNeutralButton("OK"){_,_ ->
+                val divBtn = findViewById<View>(R.id.Divide_button_view) as Button
+                val mulBtn = findViewById<View>(R.id.Multply_button_view) as Button
+                val minusBtn = findViewById<View>(R.id.Minus_button_view) as Button
+                val plusBtn = findViewById<View>(R.id.Plus_button_view) as Button
+                val signBtn = findViewById<View>(R.id.changeSign_button_view) as Button
+                divBtn.isEnabled = false
+                mulBtn.isEnabled = false
+                minusBtn.isEnabled = false
+                plusBtn.isEnabled = false
+                signBtn.isEnabled = false
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
     }
 }
